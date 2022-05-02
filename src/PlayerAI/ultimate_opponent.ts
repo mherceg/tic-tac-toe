cba23e93cf7837a59150daf31968813ac1c276a7
Matija Herceg <herc.rules@gmail.com>
2022-05-02 10:16:48 +0200

    Add ultimate opponent

--- src/PlayerAI/ultimate_opponent.ts
+++ src/PlayerAI/ultimate_opponent.ts
@@ -1,10 +1,26 @@
+import log from "loglevel";
+import TicTacToeEngine, { Player as Fplayer } from "tic-tac-toe-minimax-engine";
 import { IOpponent } from ".";
-import model from "../model";
+import { IGame, Move, Player } from "../model";
 
 
 export default class UltimateOpponent implements IOpponent{
-    move(game: model): model {
-        throw new Error("Method not implemented.");
+    move(game: IGame): IGame {
+        log.debug("Ultimate opponent making a move");
+
+        let me = game.last_move == Player.O ? Player.X : Player.O;
+
+        const aigame = new TicTacToeEngine(Fplayer.PLAYER_ONE);
+        for (let m of game.moves){
+            aigame.makeNextMove(m.x, m.y);
+        }
+
+        let {x, y} = aigame.getBestMove();
+        log.debug(`Selected move ${x}, ${y}`);
+
+        game.move(new Move(x, y, me), true);
+
+        return game;
     }
 
 }
