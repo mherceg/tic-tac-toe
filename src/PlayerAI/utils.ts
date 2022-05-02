785666246905d1ae09b56b4cc4d43d16f799638c
Matija Herceg <herc.rules@gmail.com>
2022-05-02 10:53:22 +0200

    Fix available moves

--- src/PlayerAI/utils.ts
+++ src/PlayerAI/utils.ts
@@ -1,20 +1,37 @@
 import { IGame, Player } from "../model";
+import { createAndFillTwoDArray } from "../utils";
 
+class point{
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        
+    }
+
+}
+
+//Inneficient implementation, O(size^2) time & space complexity. Make sure to improve if using larger boards.
 export function available_moves(game: IGame): [number, number][] {
 
-    let available = new Set<[number,number]>();
+    let available = createAndFillTwoDArray(game.size, game.size, true);
 
-    console.log("size", game.size);
+    for (let m of game.moves){
+        available[m.x][m.y] = false;
+    }
+
+    let ret : [number, number][] = new Array<[number, number]>();
     for (let i = 0; i < game.size; ++i){
         for (let j = 0; j < game.size; ++j){
-            available.add([i,j]);
+            if (available[i][j]){
+                ret.push([i,j]);
+            }
         }
     }
-    for (let m of game.moves){
-        available.delete([m.x, m.y]);
-    }
 
-    return Array.from(available);
+    return ret;
 }
 
 export function my_symbol(last_move: Player) : Player{
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ const main = async(storage: IStorage) => {
     let game = new Game(4, Opponent.medium);
     console.log(game.move(new Move(1, 1, Player.X)));
     console.log(game.moves);
-    // console.log(game.move(new Move()));
-    // console.log(game.move(new Move()));
-    // console.log(game.move(new Move()));
-    // console.log(game.move(new Move()));
-    // console.log(game.move(new Move()));
+    console.log(game.move(new Move(0, 0, Player.X)));
+    console.log(game.moves);
+    console.log(game.move(new Move(1, 2, Player.X)));
+    console.log(game.moves);
 }
 
 export default main;
