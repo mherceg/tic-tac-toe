0a7e3648ac5d703254c8f480dfc4c60db7d0200a
Matija Herceg <herc.rules@gmail.com>
2022-05-03 22:02:27 +0200

    Move to Apollo

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,36 @@
 import log from "loglevel";
 import Game, { Opponent, Move, Player } from "./model";
 import { IStorage } from "./storage/interface";
+import { createServer } from 'http';
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import express from "express";
+import { PubSub } from 'graphql-subscriptions';
+import { ApolloServer } from 'apollo-server-express';
 
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
-var express = require('express');
+var cors = require('cors')
 
-// `
-//     type Query {
-//         game_ids: [ID]
-//         games: [Game]
-//     },
-//     type Mutation {
-//         create_game(opponent: String) : Game
-//     },
-//     type Game {
-//         id: String
-//         moves: [Move]
-//         board: [String]
-//         size: Int
-//         winner: String
-//         opponent: String
-//         last_move: String
-//         is_finished: Boolean
-//     }
-// `
+const pubsub = new PubSub();
 
-var schema = buildSchema(`
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
     type Query {
         game_ids: [ID]
         games: [Game]
         get_history(id: String): [Move]
-    },
+    }
     type Mutation {
         create_game(opponent: String): String
         move(id: String, x: Int, y: Int, p: String): String
         opponent_start(id: String): String
-    },
+    }
+    type Subscription {
+        game_created: String
+        hello: String
+    }
     type Game {
         id: String
         size: Int
@@ -45,13 +39,13 @@ var schema = buildSchema(`
         isFinished: Boolean
         moves: [Move]
         elements: [[String]]
-    },
+    }
     type Move {
         x: Int
         y: Int
         player: String
     }
-`)
+`;
 
 const main = async(storage: IStorage, port: number) => {
 
@@ -59,74 +53,127 @@ const main = async(storage: IStorage, port: number) => {
     storage.add_game(game);
     console.log(game.move(new Move(1, 1, Player.X)));
     console.log(game.moves);
-    console.log(game.move(new Move(0, 0, Player.X)));
-    console.log(game.moves);
-    console.log(game.move(new Move(1, 2, Player.X)));
-    console.log(game.moves);
+    // console.log(game.move(new Move(0, 0, Player.X)));
+    // console.log(game.moves);
+    // console.log(game.move(new Move(1, 2, Player.X)));
+    // console.log(game.moves);
 
-    var rootValue = {
-        game_ids: () => {
-            log.info("Requested game ids");
-            return storage.games.keys();
-        },
-        games: () => {
-            log.info("Games requested");
-            log.trace(storage.games);
-            return storage.games.values();
-        },
-        get_history: (args) => {
-            log.info(`History requested for id ${id}`);
-            var id = args["id"];
-            let game = storage.get_game(id);
-            return game.moves;
-        },
-        create_game: (args) => {
-            log.debug(`Creating game ${opponent}`);
-            var opponent = args["opponent"];
-            var opp = Opponent.multiPlayer;
-            if (opponent == "easy" || opponent == "1") {
-                opp = Opponent.easy;
-            } else if (opponent == "medium" || opponent == "2") {
-                opp = Opponent.medium;
-            } else if (opponent == "hard" || opponent == "3"){
-                opp = Opponent.hard;
+    const resolvers = {
+        Query: {
+            game_ids(){
+                log.info("Requested game ids");
+                return storage.games.keys();
+            },
+            games(){
+                log.info("Games requested");
+                log.trace(storage.games);
+                return storage.games.values();
+            },
+            get_history(parent, args){
+                log.info(`History requested for id ${id}`);
+                var id = args["id"];
+                let game = storage.get_game(id);
+                return game.moves;
             }
-            let game = new Game(storage.get_id(), opp);
-            storage.add_game(game);
-            return game.id;
         },
-        move: (args) => {
-            var id = args["id"];
-            var x = args["x"];
-            var y = args["y"];
-            var p = args["p"];
-            var game = storage.get_game(id);
-            var player: Player;
-            if (p === "X"){
-                player = Player.X;
-            } else if (p === "O"){
-                player = Player.O;
+        Mutation: {
+            create_game(parent, args){
+                var opponent = args["opponent"];
+                log.debug(`Creating game ${opponent}`);
+                var opp = Opponent.multiPlayer;
+                if (opponent == "easy" || opponent == "1") {
+                    opp = Opponent.easy;
+                } else if (opponent == "medium" || opponent == "2") {
+                    opp = Opponent.medium;
+                } else if (opponent == "hard" || opponent == "3"){
+                    opp = Opponent.hard;
+                }
+                let game = new Game(storage.get_id(), opp);
+                storage.add_game(game);
+                pubsub.publish("GAMES", {game_created: game.id});
+                return game.id;
+            },
+            move(parent, args){
+                var id = args["id"];
+                var x = args["x"];
+                var y = args["y"];
+                var p = args["p"];
+                var game = storage.get_game(id);
+                var player: Player;
+                if (p === "X"){
+                    player = Player.X;
+                } else if (p === "O"){
+                    player = Player.O;
+                }
+                game.move(new Move(x, y, player));
+                return id;
+            },
+            opponent_start(parent, args){
+                var id = args["id"];
+                log.info(`Opponent start requested for ${id}`);
+                var game = storage.get_game(id);
+                game.opponent_start();
             }
-            game.move(new Move(x, y, player));
-            return id;
         },
-        opponent_start: (args) => {
-            var id = args["id"];
-            log.info(`Opponent start requested for ${id}`);
-            var game = storage.get_game(id);
-            game.opponent_start();
+        Subscription: {
+            game_created:{
+                subscribe: () => {
+                    log.info("Subscribed");
+                    return pubsub.asyncIterator(['GAMES']);
+                }
+            },
+            hello: {
+                // Example using an async generator
+                subscribe: async function* () {
+                  for await (const word of ["Hello", "Bonjour", "Ciao"]) {
+                    yield { hello: word };
+                  }
+                },
+              },
         }
     };
 
     var app = express();
-    app.use('/graphql', graphqlHTTP({
-        schema: schema,
-        rootValue: rootValue,
-        graphiql: true,
-    }));
-    app.listen(port);
-    log.info(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+    app.use(cors());
+    app.set("port", port);
+    const httpServer = createServer(app);
+    // app.use('/graphql', graphqlHTTP({
+    //     schema: schema,
+    //     rootValue: rootValue,
+    //     graphiql: true,
+    // }));
+    // app.listen(port);
+    // log.info(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+    const schema = makeExecutableSchema({typeDefs, resolvers});
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+        path: '/graphql',
+    });
 
+    const serverCleanup = useServer({ schema }, wsServer);
+    const server = new ApolloServer({
+        schema,
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            {
+            async serverWillStart() {
+                return {
+                async drainServer() {
+                    await serverCleanup.dispose();
+                },
+                };
+            },
+            },
+        ],
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+    httpServer.listen(port, () => {
+        console.log(
+            `Server is now running on http://localhost:${port}${server.graphqlPath}`,
+        );
+    });
 }
 
 export default main;
